e1b32b1f7920882e3fe1f23d6bf4aaf9dd4d099d
oluwadara <oluwadaraafolabi4@gmail.com>
2023-12-23 00:01:20 +0100

    landingPage files

---
+++ JS/dev_portfolio.js
@@ -0,0 +1,10 @@
+let navOpen = document.querySelector('#menu');
+let navClose = document.getElementById('close-btn');
+let menu = document.querySelector('nav');
+
+navOpen.addEventListener('click', function () {
+  menu.classList.add('open-nav');
+});
+navClose.addEventListener('click', function () {
+  menu.classList.remove('open-nav');
+});
